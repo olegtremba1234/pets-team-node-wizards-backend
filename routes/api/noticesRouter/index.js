f5c2eae85ac59a9757305b8656b60c625534b2d3
Ivan Drahoner <111982515+idrahoner@users.noreply.github.com>
2023-03-04 19:59:03 +0200

    Refactored aggregation by mongoose syntaxys

--- routes/api/noticesRouter/index.js
+++ routes/api/noticesRouter/index.js
@@ -8,46 +8,54 @@ const router = express.Router();
 // TODO:  add check is authorized user middleware for 'get by category'
 // TODO:  add projection to endpoints
 
+// aggregate
 router.get(
   "/by-category/:category",
   callController(noticesController.getNoticesByCategory)
 );
 
+// aggregate
 router.get(
   "/certain/:noticeId",
   callController(noticesController.getCertainNotice)
 );
 
+// default = true
 router.get(
   "/my-favorites",
   authMiddleware,
   callController(noticesController.getAllFavorites)
 );
 
-router.patch(
-  "/my-favorites/:noticeId/favorite",
+// defaul = true
+router.post(
+  "/my-favorites/:noticeId",
   authMiddleware,
   callController(noticesController.setNoticeFavorites)
 );
 
-router.patch(
-  "/my-favorites/:noticeId/unfavorite",
+// default = false
+router.delete(
+  "/my-favorites/:noticeId",
   authMiddleware,
   callController(noticesController.unsetNoticeFavorites)
 );
 
+// aggregate
 router.get(
   "/my-notices",
   authMiddleware,
   callController(noticesController.getOwnNotices)
 );
 
+// defaul = false
 router.post(
   "/my-notices/:category",
   authMiddleware,
   callController(noticesController.createOwnNotice)
 );
 
+// 204
 router.delete(
   "/my-notices/:noticeId",
   authMiddleware,
--- services/noticesService/get-by-category.js
+++ services/noticesService/get-by-category.js
@@ -1,49 +1,31 @@
 const { NoticeModel } = require("../../models");
 
 const getByCategory = async (category = null, userId = null) => {
-  const notices = await NoticeModel.aggregate([
-    {
-      $match: {
-        category: category,
-      },
-    },
-    {
-      $addFields: {
-        isFavorite: {
-          $cond: {
-            if: {
-              $first: {
-                $filter: {
-                  input: "$favoritedBy",
-                  as: "userId",
-                  cond: {
-                    $eq: [
-                      {
-                        $convert: {
-                          input: "$$userId",
-                          to: "string",
-                        },
-                      },
-                      userId,
-                    ],
-                  },
+  const notices = await NoticeModel.aggregate()
+    .match({ category })
+    .addFields({
+      isFavorite: {
+        $cond: {
+          if: {
+            $first: {
+              $filter: {
+                input: "$favoritedBy",
+                as: "favoritedUserId",
+                cond: {
+                  $eq: [
+                    { $convert: { input: "$$favoritedUserId", to: "string" } },
+                    userId,
+                  ],
                 },
               },
             },
-            then: true,
-            else: false,
           },
+          then: true,
+          else: false,
         },
       },
-    },
-    {
-      $project: {
-        owner: 0,
-        favoritedBy: 0,
-        __v: 0,
-      },
-    },
-  ]);
+    })
+    .project({ owner: 0, favoritedBy: 0, __v: 0 });
 
   return notices;
 };
