18980312ca8d32d0ffe80e0dff71f96c7f71b983
Yuliia Cherkashyna <jul11091109@gmail.com>
2023-03-10 16:12:22 +0200

    validation city

--- helpers/validation/auth/updateUser.js
+++ helpers/validation/auth/updateUser.js
@@ -12,7 +12,7 @@ const updateUser = Joi.object({
     .pattern(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(.\w{2,3})+$/),
   birthday: Joi.date().max("now").format("DD.MM.YYYY"),
   phone: Joi.string().pattern(/^\+380\d{9}$/),
-  city: Joi.string().pattern(/^([a-zA-Z-А-Яа-яЁёЇїІіЄєҐґ']+){3,}$/),
+  city: Joi.string().pattern(/^(([a-zA-Z-А-Яа-яЁёЇїІіЄєҐґ',]+){3,})*$/),
   avatarUrl: Joi.string(),
 });
 
