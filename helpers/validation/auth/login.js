439314b2e2017c5843873e78a07f96fb19286896
Yuliia Cherkashyna <jul11091109@gmail.com>
2023-03-10 22:23:02 +0200

    validation email

--- helpers/validation/auth/login.js
+++ helpers/validation/auth/login.js
@@ -2,10 +2,10 @@ const Joi = require("joi");
 
 const login = Joi.object({
   email: Joi.string()
-    .min(2)
+    .min(10)
     .max(63)
     .email()
-    .pattern(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(.\w{2,3})+$/)
+    .pattern(/^(\w+([.-]?\w+){1,})*@\w+([.-]?\w+)*(.\w{2,3})+$/)
     .required(),
   password: Joi.string().pattern(/^\S+$/).min(7).max(32).required(),
 });
--- helpers/validation/auth/register.js
+++ helpers/validation/auth/register.js
@@ -7,10 +7,10 @@ const register = Joi.object({
     .pattern(/^([a-zA-Z-А-Яа-яЁёЇїІіЄєҐґ']+){3,}$/)
     .required(),
   email: Joi.string()
-    .min(2)
+    .min(10)
     .max(63)
     .email()
-    .pattern(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(.\w{2,3})+$/)
+    .pattern(/^(\w+([.-]?\w+){1,})*@\w+([.-]?\w+)*(.\w{2,3})+$/)
     .required(),
   password: Joi.string().pattern(/^\S+$/).min(7).max(32).required(),
   city: Joi.string()
