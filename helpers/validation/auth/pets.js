7f4a89b76c80c04c09b18135373551c001a8835a
FrancuzAlexandr <francuz_s@ukr.net>
2023-03-10 23:05:25 +0200

    pets.birthday.validation

--- helpers/validation/auth/pets.js
+++ helpers/validation/auth/pets.js
@@ -1,8 +1,8 @@
-const Joi = require("joi");
+const Joi = require("joi").extend(require("@joi/date"));
 
 const addPetSchema = Joi.object({
   name: Joi.string().min(2).max(16).required(),
-  birthDay: Joi.string().required(),
+  birthDay: Joi.date().max("now").format("DD.MM.YYYY").required(),
   breed: Joi.string().min(2).max(16).required(),
   comments: Joi.string().min(8).max(120).required(),
 });
